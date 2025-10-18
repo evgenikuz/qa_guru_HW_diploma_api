39838ddc7dc89bd5c3f2a9d143abc2784355a140
Evgeniia Kharitonova <kuznetsovaevgeni@gmail.com>
2025-10-18 20:41:14 +0300

    added import

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.util.Map;
+
 public class TestBase {
 
     @BeforeAll
